efe217cb93fd00d2c2fc768926cb98f0ca310240
David Garcia <david_5043@hotmail.com>
2020-04-09 11:58:44 -0500

    First commit

---
+++ controllers/userController.js
@@ -0,0 +1,22 @@
+const User = require("../models/User")
+
+exports.home = (req, res) => {
+  res.render("home-guest")
+}
+
+exports.register = (req, res) => {
+  let user = new User(req.body)
+  user.register()
+  if (user.errors.length) {
+    res.send(user.errors)
+  } else {
+    res.send("Congrats! there are no errors")
+  }
+}
+
+exports.login = function (req, res) {
+  let user = new User(req.body)
+  user.login()
+    .then((result) => res.send(result))
+    .catch((err) => res.send(err))
+}
